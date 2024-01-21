bfb91202666cc648f236abcc00589baf906f6892
Bashirov <bgbashirov@gmail.com>
2024-01-21 15:25:01 +0400

    working command parser

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 import { parseArguments } from "./cmdparser.js";
 
-const args = await parseArguments(process.argv.slice(2))
+const args = parseArguments(process.argv.slice(2))
 console.log(args)
--- src/cmdparser.js
+++ src/cmdparser.js
@@ -1,7 +1,44 @@
 import minimist from 'minimist'
+import RJSON from 'relaxed-json'
+
+const validMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+function validateMethod(method) {
+    if (!validMethods.includes(method)) {
+        throw Error(`${method} is not a HTTP method`)
+    }
+}
+
+function parseJSONArgument(argString) {
+    if ((argString[0] == "{") && (argString[argString.length - 1] == "}")) {
+        return RJSON.parse(argString)
+    } else {
+        return RJSON.parse("{" + argString + "}")
+    }
+}
 
 function parseArguments(s) {
-   return minimist(s)
+    const args = minimist(s, {
+        alias: {
+            'h': 'header',
+            'b': 'body'
+        }
+    })
+
+    const method = args["_"][0]
+    validateMethod(method)
+
+    const url = args["_"][1]
+
+    const header = args['header'] ? parseJSONArgument(args['header']) : null
+    const body = args['body'] ? parseJSONArgument(args['body']) : null
+
+    return {
+        method,
+        url,
+        header,
+        body
+    }
 }
 
 export { parseArguments };
