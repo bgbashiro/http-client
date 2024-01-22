1ef5bc3be601f619887a16d8be2fe5c94c0b730c
Bashirov <bgbashirov@gmail.com>
2024-01-22 13:53:55 +0400

    POST request handler working

--- src/requesthandler.js
+++ src/requesthandler.js
@@ -16,18 +16,50 @@ async function handleGETRequest(url) {
     }
 
     return {
-        status, 
+        status,
         content
     }
 }
 
-function handlePOSTRequest(request) {
-    return null
+function prepareBody(contentType, body) {
+    if (contentType=="application/json") {
+        return JSON.stringify(body)
+    } else if (contentType=="application/x-www-form-urlencoded") {
+        return new URLSearchParams(body).toString()
+    } else {
+        return body
+    }
+}
+
+async function handlePOSTRequest(url, header, body) {
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: header,
+        body: prepareBody(header['Content-type'], body)
+    })
+    let status = response.status;
+    let content = await response.text();
+
+    try {
+        content = RJSON.parse(content.trim());
+    } catch (error) {
+        if (!(error instanceof SyntaxError)) {
+            console.log(error)
+        }
+        content = content.trim()
+    }
+
+    return {
+        status,
+        content
+    }
 }
 
 function handleRequest(request) {
     if (request.method == "GET") {
         return handleGETRequest(request.url);
+    } else if (request.method == "POST") {
+        return handlePOSTRequest(request.url, request.header, request.body)
     } else {
         throw Error(`${request.method} not implemented`);
     }
